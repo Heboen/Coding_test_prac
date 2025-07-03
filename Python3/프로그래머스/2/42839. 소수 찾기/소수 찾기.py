5c31935f31704c8f237a49f9a0941eff7a2acc6a
Heboen <121006630+Heboen@users.noreply.github.com>
2025-07-03 13:36:33 +0900

    [level 2] Title: 소수 찾기, Time: 6.99 ms, Memory: 9.47 MB -BaekjoonHub

--- Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -2,8 +2,8 @@ from itertools import permutations
 import math
 
 def isPrime(n):
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n%i == 0:
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
             return False
     return True
 
@@ -12,15 +12,15 @@ def solution(numbers):
     num = 0
     num_arr = set()
     
-    for i in range(1, len(numbers)+1):
+    for i in range(1,len(numbers)+1):
         for permutation in permutations(numbers,i):
             num = int("".join(permutation))
             num_arr.add(num)
-            
+    
     for i in list(num_arr):
         if i == 1 or i == 0:
             continue
         if isPrime(i):
-            answer += 1
-
+            answer +=1
+    
     return answer
