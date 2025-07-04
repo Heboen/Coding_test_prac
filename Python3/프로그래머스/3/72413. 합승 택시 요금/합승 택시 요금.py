2a9aefd03c1ded6893bb33fb42ae64c62275ddb3
Heboen <121006630+Heboen@users.noreply.github.com>
2025-07-04 10:03:29 +0900

    [level 3] Title: 합승 택시 요금, Time: 1631.04 ms, Memory: 13.1 MB -BaekjoonHub

--- Python3/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ Python3/프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,6 +1,6 @@
 def solution(n, s, a, b, fares):
     answer = 100001*n
-    M = 100001 * n
+    M = 100001*n
     graph = [[M for _ in range(n+1)] for _ in range(n+1)]
     
     for i,j,c in fares:
@@ -9,16 +9,15 @@ def solution(n, s, a, b, fares):
         
     for i in range(1,n+1):
         for j in range(1,n+1):
-            if i==j:
+            if i == j:
                 graph[i][j] = 0
                 
     for k in range(1,n+1):
         for i in range(1,n+1):
             for j in range(1,n+1):
-                if graph[i][j] > graph[i][k]+graph[k][j]:
-                    graph[i][j] = graph[i][k]+graph[k][j]
-    
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
+                    
     for i in range(1,n+1):
         answer = min(answer, graph[i][s]+graph[i][a]+graph[i][b])
-        
     return answer
