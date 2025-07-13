a4e279862ffd60596d5d736f0e0939ca1336f909
Heboen <121006630+Heboen@users.noreply.github.com>
2025-07-13 21:21:22 +0900

    [level 3] Title: 징검다리 건너기, Time: 437.68 ms, Memory: 17.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,20 @@
+def solution(stones, k):
+    answer = 0
+    left, right, cnt = 1, max(stones), 0
+    
+    while left <= right:
+        cnt = 0
+        mid = (left + right) // 2
+        for stone in stones:
+            if(stone - mid) <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt>= k:
+                break
+        if cnt<k:
+            left = mid + 1
+        else:
+            answer = mid
+            right = mid -1
+    return answer
