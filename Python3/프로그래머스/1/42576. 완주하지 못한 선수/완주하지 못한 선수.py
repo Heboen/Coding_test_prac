6165933da7bfefab5acde4e1ee86a82e0d97ff47
Heboen <121006630+Heboen@users.noreply.github.com>
2025-07-13 21:22:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.87 ms, Memory: 33.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,10 @@
 def solution(participant, completion):
-    answer = ''
-    person = dict()
-    for i in participant:
-        person[i] = person.get(i,0) + 1
-    for i in completion:
-        person[i] -= 1
-    for i in person:
-        if person[i] != 0:
+    cnt={} #참여자 이름의 등장 횟수를 저장하기 위한 딕셔너리
+    for i in participant: #참여자 명단 순회
+        cnt[i] = cnt.get(i,0) + 1 #참여자 명단에 이름이 등장하므로 +1
+    for j in completion: #완주자 명단 순회
+        cnt[j] -= 1 #완주자 명단에 이름이 있다면 -1
+    for i,cnt in cnt.items():#딕셔너리 순회
+        if(cnt != 0): #딕셔너리의 값이 0이 아니라면 완주자 명단에 이름이 없다는 것이므로 0이 아닌 해당 이름을 리턴
             return i
+    
