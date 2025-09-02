2121ac019aed84e6977f3199c3abc82659c5a397
Heboen <121006630+Heboen@users.noreply.github.com>
2025-09-02 19:05:18 +0900

    [level 2] Title: 타겟 넘버, Time: 641.53 ms, Memory: 9.23 MB -BaekjoonHub

--- Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,20 @@
+
 def solution(numbers, target):
-    answer = dfs(numbers,target,0)
+    answer = dfs(numbers, target,0)
     return answer
+
 def dfs(numbers,target,depth):
-    answer = 0
+    answer = 0 
+    
     if depth == len(numbers):
         if sum(numbers) == target:
             return 1
-        else: return 0
+        else:
+            return 0
+        
     else:
-        answer += dfs(numbers,target, depth+1)
+        answer += dfs(numbers, target, depth+1)
         numbers[depth] *= -1
-        answer += dfs(numbers,target, depth+1)
-        return answer
+        answer += dfs(numbers, target, depth+1)
+        return answer 
+    
