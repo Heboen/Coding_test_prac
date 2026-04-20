225842c6239599c7d8ca3dcf3686dd799f7327be
Heboen <121006630+Heboen@users.noreply.github.com>
2026-04-20 19:11:58 +0900

    주석 수정
    Signed-off-by: Heboen <121006630+Heboen@users.noreply.github.com>

--- Java/프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ Java/프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,19 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
+        int answer = 0; 
         Map<Integer, Integer> map = new HashMap<>();
+        //귤 크기별 수확량 저장할 해시맵
         
         for (int i : tangerine){
             map.put(i, map.getOrDefault(i,0) + 1);
+            //map에 귤 크기별 갯수 저장
+            //map에 귤이 없다면 0을 있다면 해당 귤의 갯수를 불러와 +1
         }
         
         List<Integer> keyList = new ArrayList<>(map.keySet());
+        //키값만 리스트 불러옴
         
         keyList.sort(((o1,o2) -> map.get(o2) - map.get(o1)));
+        //keyList를 귤 갯수에 따라 내림차순 정렬
         
         for (int i : keyList){
-            if(k<=0){
+            if(k<=0){//귤을 다 골랐다면 탈출
                 break;
             }
             answer++;
