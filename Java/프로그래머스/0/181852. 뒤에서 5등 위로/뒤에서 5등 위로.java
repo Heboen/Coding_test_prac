29aa9e2d64f8049cea9ed1f15310c5cf5a6a0bb3
Heboen <121006630+Heboen@users.noreply.github.com>
2026-07-24 09:55:39 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 4.38 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length-5];
+        Arrays.sort(num_list);
+        int len = num_list.length;
+        int idx = 0;
+        for(int i = 5; i < len; i++){
+            answer[idx] = num_list[i];
+            idx++;
+        }
+        return answer;
+    }
+}
